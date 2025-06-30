1655ad7a0671611290879952e4ac06ee3db12be4
Dhayanithi132471 <717822l209@kce.ac.in>
2025-06-30 11:35:30 +0530

    hello

---
+++ app.js
@@ -0,0 +1,5 @@
+function sayHello() {
+  var name = document.getElementById("nameInput").value;
+  var greeting = "Hello, " + name + "! 💀";
+  document.getElementById("greeting").textContent = greeting;
+}
